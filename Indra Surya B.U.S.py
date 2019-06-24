21a779e1cf30c4fbd4c475738bacf17cedcf909b
IndraSuryaBudiUtomoSasmito <49376260+IndraSuryaBudiUtomoSasmito@users.noreply.github.com>
2019-06-24 11:19:27 +0700

    Penambahan Uas

--- Indra Surya B.U.S.py
+++ Indra Surya B.U.S.py
@@ -2,7 +2,7 @@ def Penjumlahan():
 	hasil=500+200
 	return hasil
   
-def Penjumlahan(args):
+def Penjumlahan(*args):
 	hasil=500+200
 	return hasil
   
@@ -10,12 +10,12 @@ def Pengurangan():
 	hasil=700-190
 	return hasil
   
-def Pengurangan(args):
+def Pengurangan(*args):
 	hasil=700-190
 	return hasil
   
 def Tampil(func,func1):
-	print "Anto memiliki bebek "func()" ekor di Mataram"
+	print "Anto memiliki bebek ",func()" ,ekor di Mataram"
 	print "Bebek anto pada tahun 2013 menjadi "func1()" karena mati keracunan"
 
 def Utama():
